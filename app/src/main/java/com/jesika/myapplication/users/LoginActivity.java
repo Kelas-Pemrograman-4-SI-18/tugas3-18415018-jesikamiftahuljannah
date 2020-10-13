d3fc8b28b778011986a0b144d5941c54192397fd
jesikamiftahull <jesika.18415018@student.ubl.ac.id>
2020-10-13 15:23:37 +0700

    tugas3

---
+++ app/src/main/java/com/jesika/myapplication/users/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.jesika.myapplication.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.jesika.myapplication.R;
+import com.ornach.nobobutton.NoboButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnPindah;
+    NoboButton btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+
+        btnPindah= (Button) findViewById(R.id.btnPindah);
+        btnLogin = (NoboButton) findViewById(R.id.btnLogin);
+
+
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
